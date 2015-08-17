db284d5f486ead41869930d4dd891ef533ca8163
bmacheski <bwm0007@gmail.com>
2015-08-16 21:14:49 -0500

    replace search autocomplete with custom directive

---
+++ src/js/search.directive.js
@@ -0,0 +1,23 @@
+'use strict';
+
+angular
+  .module('angularMovie')
+  .directive('navSearch', navSearch);
+
+  function navSearch($http, API_URL, KEY) {
+    return {
+      restrict: 'E',
+      templateUrl: 'views/navsearch.html',
+      link: function(scope, elem, attrs) {
+        scope.searchresults;
+        scope.search = function() {
+          var url = API_URL + `/search/movie?api_key=${KEY}&query=`;
+          $http
+            .get(url + scope.searchText)
+            .success(function(res) {
+              scope.searchresults = res.results;
+            })
+        }
+      }
+    }
+  }
